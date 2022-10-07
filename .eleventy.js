4af0a497890e768810c6aed72e756f5a559267f0
Rascode <justin@rascode.dev>
2022-10-07 13:02:32 -0400

    Multiple changes in flow state.  I'll do better in future.

--- .eleventy.js
+++ .eleventy.js
@@ -1,17 +1,28 @@
+//imports
+const pluginWebc  = require("@11ty/eleventy-plugin-webc");
+const pluginRss   = require("@11ty/eleventy-plugin-rss");
+
+//config function
 module.exports = function(eleventyConfig) {
+
+    //passthroughs
     eleventyConfig.addPassthroughCopy("manifest.json");
     eleventyConfig.addPassthroughCopy(".prettierrc");
-    eleventyConfig.addPassthroughCopy({"src/assets/images" : "/assets/img"});
-    eleventyConfig.addPassthroughCopy({"src/assets/fonts" : "/assets/fonts"});
-    eleventyConfig.addPassthroughCopy("src/styles");
+    eleventyConfig.addPassthroughCopy("src/assets/");
+    eleventyConfig.addPassthroughCopy({"src/styles" : "/styles"});
+    
+    //plugins
+    eleventyConfig.addPlugin(pluginWebc);
+    eleventyConfig.addPlugin(pluginRss);
+
 
     return {
       dir:{
-        input: "src/content",        
-        data: "../data",
-        includes: "../templates/partials",
-        layouts: "../templates/layouts",
-        output: "dist"
+        input:    "src",        
+        data:     "data",
+        includes: "templates/includes",
+        layouts:  "templates/layouts",
+        output:   "dist"
       },
       dataTemplateEngine: "njk",
       markdownTemplateEngine: 'njk',
